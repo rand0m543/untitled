00ea0079e10ecb7cc28519aec5ea240b20ae2ef6
rand0m543 <ererror102@gmail.com>
2026-03-24 10:05:35 +0000

    first commit
    first commit for this

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+// Main.java
 public class Main {
     public static void main(String[] args) {
         // lol
         System.out.println("Hello, World!");
+        // Create a course
+        Course course = new Course("Computer Science");
+
+        // Create students
+        Student student1 = new Student("Alice", 20);
+        Student student2 = new Student("Bob", 22);
+
+        // Enroll students in the course
+        course.addStudent(student1);
+        course.addStudent(student2);
+
+        // Display course details
+        course.displayStudents();
     }
 
     int 5 = new int
