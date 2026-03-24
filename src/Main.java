499ab09ab130a71168e0b2063fa5ec983414d9f1
rand0m543 <ererror102@gmail.com>
2026-03-24 12:19:57 +0000

    main changed again
    nothing interesting was changed but it was changed again

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 // Main.java
 public class Main {
     public static void main(String[] args) {
+
         // lol
         System.out.println("Hello, World!");
         // Create a course
         Course course = new Course("Computer Science");
         
+        Course course = new Course("Computer Science"); // hh
+
         Student student1 = new Student("Alice", 20);
         Student student2 = new Student("Bob", 22);
 
