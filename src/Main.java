2d5837f046d777e871ae7a043d9d3c00d7bb0fe2
rand0m543 <ererror102@gmail.com>
2026-03-24 09:58:16 +0000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // lol
+        System.out.println("Hello, World!");
+    }
+
+    int 5 = new int
+    // die
+    // please
+}
